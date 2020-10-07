7ce821a5989ee4933ba89d09c0bd5447bb73fd28
Raythan Machado <raythan.machado@sinqia.com.br>
2020-10-07 13:43:37 -0300

    Ajustes

--- to_post_application/lib/main.dart
+++ to_post_application/lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
-//import 'package:to_post_application/telas/crud_example.dart';
-import 'package:to_post_application/telas/principal.dart';
+import 'package:to_post_application/entities/entity_sample.dart';
+import 'package:to_post_application/resources/database_context.dart';
+//import 'package:to_post_application/telas/principal.dart';
 
-void main() => runApp(Principal());
+//oid main() => runApp(Principal());
 
-// void main() => runApp(new MyApp());
-// final routes = {
-//   '/home': (BuildContext context) => new HomePage(),
-//   '/': (BuildContext context) => new HomePage(),
-// };
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MemoDbProvider memoDb = MemoDbProvider();
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'Sqflite App',
-//       theme: new ThemeData(primarySwatch: Colors.teal),
-//       routes: routes,
-//     );
-//   }
+  final memo = MemoModel(
+    id: 1,
+    title: 'Title 1',
+    content: 'Note 1',
+  );
+
+  await memoDb.addItem(memo);
+  var memos = await memoDb.fetchMemos();
+  print(memos[0].title); //Title 1
+
+// void forEach(void f(var element)) {
+//   for (var element in memos) f(element);
 // }
+
+  for (var element in memos) {
+    print(element.title);
+  }
+
+  final newmemo = MemoModel(
+    id: memo.id,
+    title: 'Title 1 changed',
+    content: memo.content,
+  );
+
+  await memoDb.updateMemo(memo.id, newmemo);
+  var updatedmemos = await memoDb.fetchMemos();
+  print(updatedmemos[0].title); //Title 1 changed
+
+  await memoDb.deleteMemo(memo.id);
+  print(await memoDb.fetchMemos()); //[]
+}
